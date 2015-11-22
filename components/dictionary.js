c34e6ca3e436c270c3ff9fa57c2a2d1180f2d063
w2moon <w2moon@gmail.com>
2015-11-22 23:56:05 +0800

    dict add

--- components/dictionary.js
+++ components/dictionary.js
@@ -15,48 +15,86 @@ module.exports = function(obj,next){
                 }
                 var $ = window.$;
 
-                $('.result-info .info-article').each(function(index){
-                    switch(index){
-                        case 0:
-                            //base info
-                            ret.name = $(this).find(".base-word").find(".word-text").text();
-                            ret.speak = [];
-                            ret.level = [];
-                            ret.desc = [];
-                            ret.change = [];
-                            $(this).find(".base-speak").children("span").each(function(){
-                                ret.speak.push($(this).text());
+                var result = $('.result-info');
+                ret.speak = [];
+                ret.level = [];
+                ret.desc = [];
+                ret.change = [];
+                ret.relationship = [];
+                ret.analyze = [];
+                ret.english = [];
+
+                //base info
+                var base = result.first();
+                ret.name = base.find(".base-word").find(".word-text").text();
+                               base.find(".base-speak").children("span").each(function(){
+                    ret.speak.push($(this).text());
+                });
+                base.find(".base-level").children("span").each(function(){
+                    ret.level.push($(this).text());
+                });
+                base.find(".base-list").children("li").each(function(){
+                    if($(this).hasClass('change')){
+                        $(this).find("p").children("span").each(function(){
+                            ret.change.push({
+                                type:$(this).text(),
+                                desc:$(this).next().text()
                             });
-                            $(this).find(".base-level").children("span").each(function(){
-                                ret.level.push($(this).text());
-                            });
-                            console.log("ddff")
-                            console.log($(this).find("ul.base-list li"))
-                            $(this).find(".base-list").children("li").each(function(){
-                                console.log("list")
-                                if($(this).hasClass('change')){
-                                    $(this).find("p").children("span").each(function(){
-                                        ret.change.push({
-                                            type:$(this).text(),
-                                            desc:$(this).next().text()
-                                        });
-                                    })
-                                }
-                                else{
-                                    console.log("???");
-                                    ret.desc.push({
-                                        type:$(this).find("span").text(),
-                                        desc:$(this).find("p").text().match(/[^\n\s]+/g).join('')
-                                    });
-                                }
-                            });
-                            break;
+                        })
+                    }
+                    else{
+                        ret.desc.push({
+                            type:$(this).find("span").text(),
+                            desc:$(this).find("p").text().match(/[^\n\s]+/g).join('')
+                        });
                     }
                 });
 
-                next("suc");
+                //english desc
+                var english = result.find(".info-article:eq(4)");
+                english.find('.article').each(function(){
+                     var article = {};
+                     article.name = $(this).find('.section-h p span').text();
+                     article.desc = [];
+                     $(this).find('.section-prep .prep-order p').each(function(){
+                          article.desc.push($(this).text())
+                     });
+                     ret.english.push(article);
+                });
 
-                console.log(ret)
+                //synonoym and antonym
+                result.find('.opposite-word').each(function(){
+                     var relation = {};
+                     relation.name = $(this).first().text();
+                     relation.desc = [];
+                     var cur = $(this).next();
+                     while(cur.hasClass('collins-section')){
+                         cur.children('.section-prep').each(function(){
+                             var desc = {};
+                             desc.name = $(this).find('.prep-order .weight-bold').text().match(/[^\n\s]+/g).join('');
+                             desc.words = [];
+                             $(this).find('.sentence-item p a').each(function(){
+                                desc.words.push($(this).text());
+                             });
+                             relation.desc.push(desc);
+                         });
+                          cur = cur.next();
+                     }
+                     ret.relationship.push(relation);
+
+                });
+
+                //analyze
+                result.find('.prep-order .text-sentence .item').each(function(){
+                     $(this).find('a').each(function(){
+                         ret.analyze.push({
+                             name:$(this).text(),
+                             desc:$(this).next().text()
+                         })
+                     });
+                });
+                next(ret);
+
                 window.close();
             });
 
